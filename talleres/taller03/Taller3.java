e4d6dcb263f8c6167be303f20d886056c0b688ea
manuelaguarnizos <52968544+manuelaguarnizos@users.noreply.github.com>
2019-07-31 23:31:08 -0500

    Add files via upload

---
+++ talleres/taller03/Taller3.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Taller3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Taller3 {
+    public static void main(String[] args) 
+    {
+        p2.per("abcd",0,0);          
+        
+    } 
+}
