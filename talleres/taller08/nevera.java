dd6dc2fc2946411e7427063ade732a85df76865f
elmigue1 <52968601+elmigue1@users.noreply.github.com>
2019-09-11 23:47:34 -0500

    Add files via upload

---
+++ talleres/taller08/nevera.java
@@ -0,0 +1,27 @@
+public class nevera 
+{
+    int des;
+    int cod;
+    public nevera(int a,int b)
+    {
+        des=a;
+        cod=b;
+    }
+
+    public int getDes() {
+        return des;
+    }
+
+    public void setDes(int des) {
+        this.des = des;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+    
+}
---
+++ talleres/taller08/p2.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Stack;
+public class p2 
+{
+    public void p2(Stack<nevera> a,ArrayList<sol> b)
+    {
+        for(int i=0;i<b.size();i++)
+        {
+        int j=0;
+        while(b.get(i).getN()<=j)
+        {
+            a.pop();
+        }
+    }
+    }
+    public Stack creador()
+    {
+        Stack<nevera> a=new Stack();
+        for(int i=0;i<300;i++)
+        {
+            nevera b=new nevera((int)(Math.random()*100),(int)Math.random()*100);
+            a.push(b);
+        }
+        return a;
+    }
+    public Stack creadors()
+    {
+        Stack<sol> a=new Stack();
+        for(int i=0;i<300;i++)
+        {
+            sol b=new sol((int)(Math.random()*100),(int)Math.random()*100);
+            a.push(b);
+        }
+        return a;
+    }
+}
+
